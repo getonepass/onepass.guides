220a00eb70d19a037473869602fcb96a97713845
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-12 22:19:35 +0100

    Update documentation for verifications and credentials, enhance badge display, and add new documents management section

--- _meta.tsx
+++ _meta.tsx
@@ -1,11 +1,10 @@
 import type { MetaRecord } from "nextra"
 export default {
   "getting-started-bridge": "Get started with OnePass Bridge",
-  verifications: "Verifications",
+  verifications: "Verifications and Credentials",
   "verifiable-credentials": "Verifiable Credentials",
   "applying-to-opportunities": "Applying to Funding Opportunities",
   "pitch-relay": {
     title: "Pitch Relay",
-    display: "hidden",
   },
 } satisfies MetaRecord
