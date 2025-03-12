8913c45cbd1a7b1c6c4ac77421fbd3e40d4297e2
Jorge Fernández <jorge.fernandez@cores.gl>
2025-03-12 12:04:11 +0100

    Add metadata and improve documentation for opportunities and verifiable credentials

---
+++ _meta.tsx
@@ -0,0 +1,11 @@
+import type { MetaRecord } from "nextra"
+export default {
+  "getting-started-bridge": "Get started with OnePass Bridge",
+  verifications: "Verifications",
+  "verifiable-credentials": "Verifiable Credentials",
+  "applying-to-opportunities": "Applying to Funding Opportunities",
+  "pitch-relay":{
+    title: "Pitch Relay",
+    display: "hidden"
+  }
+} satisfies MetaRecord;
---
+++ applying-to-opportunities/_meta.tsx
@@ -0,0 +1,7 @@
+import type { MetaRecord } from "nextra"
+export default {
+  search: "Searching for opportunities",
+  scope: "Types of opportunities",
+  description: "About the opportunity profiles",
+  applying: "Applying with OnePass",
+} satisfies MetaRecord;
